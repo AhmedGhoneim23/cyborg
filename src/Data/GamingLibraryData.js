ed348faf0d99feb6ba29198671ebce5af6d34cb0
AhmedGhoneim23 <108593418+AhmedGhoneim23@users.noreply.github.com>
2023-05-02 07:24:52 +0300

    Update GamingLibraryData.js

--- src/Data/GamingLibraryData.js
+++ src/Data/GamingLibraryData.js
@@ -2,9 +2,6 @@ import GamingLibrary_01 from '../assets/images/game-01.jpg'
 import GamingLibrary_02 from '../assets/images/game-02.jpg'
 import GamingLibrary_03 from '../assets/images/game-03.jpg'
 
-
-
-
 const GamingLibraryData = [
   { id: "library_item_1", image: GamingLibrary_01, title:'Dota2', category:'Legendary', date_added: '23/05/2024', hours_played:'770 H 22 Mins', download: 'Downlaoded' },
   { id: "library_item_2", image: GamingLibrary_02, title:'Fortnite', category:'Sandbox', date_added: '23/05/2024', hours_played:'770 H 22 Mins', download: 'Downlaoded' },
